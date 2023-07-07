7c5ba6146284a8666a53762499d5afdab19fcb16
Avinash Biradar <biradarav.100@gmail.com>
2023-07-07 12:56:10 +0530

    remove screen object

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, render} from "@testing-library/react";
 import Board from "./Board";
 import { shallow } from "enzyme";
 
